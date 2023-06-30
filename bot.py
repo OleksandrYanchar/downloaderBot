1907bdac132f79a8284bcb6496db35d722df2d6d
yurii <yuriydorosh2005@gmail.com>
2023-06-30 13:41:51 +0300

    Refactoring code with black and flake8

--- bot.py
+++ bot.py
@@ -1,11 +1,14 @@
 import telebot
-from settings import  TOKEN
+from settings import TOKEN
+
 # Замініть "YOUR_TOKEN" на власний токен, отриманий від BotFather
 bot = telebot.TeleBot(TOKEN)
+
 
 @bot.message_handler(func=lambda message: True)
 def echo(message):
     bot.reply_to(message, message.text)
+
 
 # Запуск бота
 bot.polling()
